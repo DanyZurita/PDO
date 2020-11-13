dee8a72155a469279918eeb25cb9816b1b4f57eb
dzurita <dzurita@cifpfbmoll.eu>
2020-11-13 16:32:30 +0100

    feat(index): Añadir lógica php para printar el index con un select en la bd

--- index.php
+++ index.php
@@ -24,7 +24,35 @@
             <h1>Read Products</h1>
         </div>
      
-        <!-- PHP code to read records will be here -->
+        <?php
+        // include database connection
+        include 'config/database.php';
+        
+        // delete message prompt will be here
+        
+        // select all data
+        $query = "SELECT id, name, description, price FROM products ORDER BY id DESC";
+        $stmt = $con->prepare($query);
+        $stmt->execute();
+        
+        // this is how to get number of rows returned
+        $num = $stmt->rowCount();
+        
+        // link to create record form
+        echo "<a href='create.php' class='btn btn-primary m-b-1em'>Create New Product</a>";
+        
+        //check if more than 0 record found
+        if($num>0){
+        
+            // data from database will be here
+            
+        }
+        
+        // if no records found
+        else{
+            echo "<div class='alert alert-danger'>No records found.</div>";
+        }
+        ?>
          
     </div> <!-- end .container -->
      
