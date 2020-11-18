7b3c967353dc0163995c24af255910fee9b2efdd
dzurita <dzurita@cifpfbmoll.eu>
2020-11-18 16:35:49 +0100

    feat(index): Paginación con acumulador

--- index.php
+++ index.php
@@ -27,6 +27,15 @@
         <?php
         // include database connection
         include 'config/database.php';
+        // PAGINATION VARIABLES
+        // page is the current page, if there's nothing set, default is page 1
+        $page = isset($_GET['page']) ? $_GET['page'] : 1;
+        
+        // set records or rows of data per page
+        $records_per_page = 5;
+        
+        // calculate for the query LIMIT clause
+        $from_record_num = ($records_per_page * $page) - $records_per_page;
         
         $action = isset($_GET['action']) ? $_GET['action'] : "";
  
