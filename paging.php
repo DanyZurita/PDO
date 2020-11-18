a54cfede63f802efc413b29664b21e3824992eef
dzurita <dzurita@cifpfbmoll.eu>
2020-11-18 16:56:27 +0100

    feat(paging): Botón primera página

--- paging.php
+++ paging.php
@@ -1,7 +1,16 @@
 <?php
 echo "<ul class='pagination pull-left margin-zero mt0'>";
  
-// first page button will be here
+// first page button
+if($page>1){
+ 
+    $prev_page = $page - 1;
+    echo "<li>";
+        echo "<a href='{$page_url}page={$prev_page}'>";
+            echo "<span style='margin:0 .5em;'>«</span>";
+        echo "</a>";
+    echo "</li>";
+}
  
 // clickable page numbers will be here
  
